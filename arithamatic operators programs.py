27f73f0488d4fa271e333fbc8e516f1b408b639c
VINAYAKA-CODE <vijiyavinay352@gmail.com>
2025-07-10 13:06:04 +0530

    Add files via upload

---
+++ arithamatic operators programs.py
@@ -0,0 +1,18 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=10
+>>> b=8
+>>> print(a+b)
+18
+>>> print(a-b)
+2
+>>> print(a*b)
+80
+>>> print(a/b)
+1.25
+>>> print(a//b)
+1
+>>> print(a**b)
+100000000
+>>> print(a%b)
+2
